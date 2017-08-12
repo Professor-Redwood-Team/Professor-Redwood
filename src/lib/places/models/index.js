5d7a8b57c9972710aa640314d7a09040cf147398
alan <alanleunglee@gmail.com>
2017-08-12 12:12:28 -0700

    removes relevance in Location model -- non breaking change

--- src/lib/places/models/index.js
+++ src/lib/places/models/index.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const LocationSchema = new mongoose.Schema({
   channel: { type: String, lowercase: true },
   query: { type: String, lowercase: true },
-  result: { type: String, lowercase: true },
-  relevance: { type:  Number }
+  result: { type: String, lowercase: true }
 })
 
 module.exports = mongoose.model('Location', LocationSchema)
