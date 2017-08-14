be8369bd48e281cbb97499b629bb643f5def94c2
alan <alanleunglee@gmail.com>
2017-08-14 13:25:42 -0700

    adds models for Raid and Group

---
+++ src/lib/raids/models/Raid.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose') 
+
+const RaidSchema = new mongoose.Schema({
+  id: { type: Number, default: 0 },
+  pokemon: { type: String, lowercase: true },
+  expiration_time: { type: Number },
+  location: { type: String},
+  channel: { type: String }
+})
+
+module.exports = mongoose.model('Raid', RaidSchema)
