f89cffbabcbdf1dc2ba1a21f414bca5a3192b1e8
alan <alanleunglee@gmail.com>
2017-10-20 13:00:38 -0700

    adds mongoose

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@
 
 const client = require('./src/client');
 const config = require('./config/secrets.json');
+const mongoose = require('mongoose')
 
 const token = config.discord.token;
 /* const db = mySQL.createConnection(config.mysql);
@@ -12,5 +13,14 @@ db.connect((err) => {
 	if (err) throw err;
 	console.log('Database Connected!');
 }); */
+
+if (config.DATABASE_URL !== null) {
+	mongoose.Promise = bluebird
+	mongoose.connect(config.DATABASE_URL, { useMongoClient: true });
+	mongoose.connection
+		.once('open', () => console.log('Database Connected!'))
+		.on('error', error => console.log(`Database connection error: ${error}`));
+}
+
 
 client.login(token);
