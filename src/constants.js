813cbbb7ca3a4c2a2b25240c3d12cafe14d92169
pbk01 <pbkeith@hotmail.com>
2017-11-22 03:30:19 -0800

    Added support for optional parameter 'sponsored' or 'park' in the !raid command.  Updated README.md for setup of 'sponsored' and 'park' roles  Updated raid.js    Revised warning messages to match new command syntax    Added section to detect optional parameter keyWords    Revised location details parsing to adjust for any detected keyWords  Updated constants.js    Added SPECIALRAIDS consants for the 'sponsored', 'park' parameters.

--- src/constants.js
+++ src/constants.js
@@ -40,6 +40,7 @@ const data = {
 	RAIDMONS: ['alakazam', 'blastoise', 'charizard', 'gengar', 'lapras', 'machamp', 'rhydon', 'snorlax', 'tyranitar', 'venusaur'],
 	LEGENDARYMONS: ['legendary', 'articuno', 'moltres', 'zapdos', 'mew', 'mewtwo', 'lugia', 'ho-oh', 'celebi', 'entei', 'raikou', 'suicune'],
 	SPECIALMONS: ['sponsored', 'highiv', 'finalevo'],
+	SPECIALRAIDS: ['sponsored', 'park'],
 	REGIONS: regionsConfig.regions,
 	COMMON_MISSPELLINGS: {
 		'hooh': 'ho-oh',
