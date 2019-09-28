127e1cd0fe58ce3d1a3dd8fbb775ca4f5b161a60
Jeff <jeffreycleu@gmail.com>
2019-09-28 03:26:57 -0700

    Converted tabs to spaces

--- src/helper.js
+++ src/helper.js
@@ -16,15 +16,15 @@ const cleanUpDetails = (detail) => {
  * @returns {string}
  */
 const formatTime = dateObj => {
-	let hour = dateObj.getHours();
-	let minute = dateObj.getMinutes();
-	const amPM = hour > 11 ? 'pm' : 'am';
-	if (hour > 12) {
-		hour -= 12;
-	} else if (hour === 0) {
-		hour = '12';
-	}
-	if (minute < 10) minute = `0${minute}`;
+  let hour = dateObj.getHours();
+  let minute = dateObj.getMinutes();
+  const amPM = hour > 11 ? 'pm' : 'am';
+  if (hour > 12) {
+    hour -= 12;
+  } else if (hour === 0) {
+    hour = '12';
+  }
+  if (minute < 10) minute = `0${minute}`;
   return `${hour}:${minute}${amPM}`;
 };
 
@@ -35,8 +35,8 @@ const formatTime = dateObj => {
  */
 const getEndTime = minutesLeft => {
   const date = new Date();
-	date.setMinutes(date.getMinutes() + minutesLeft);
-	return formatTime(date);
+  date.setMinutes(date.getMinutes() + minutesLeft);
+  return formatTime(date);
 };
 
 /**
@@ -47,13 +47,13 @@ const getEndTime = minutesLeft => {
  * @returns {string}
  */
 const getLegendaryTag = (boss, legendaries, data) => {
-	if (legendaries.includes(boss)) {
-		if (data.rolesByName['legendary']) {
-			return '<@&' + data.rolesByName['legendary'].id + '> ';
-		} else {
-			console.warn('Please create a role called legendary.');
-		}
-	}
+  if (legendaries.includes(boss)) {
+    if (data.rolesByName['legendary']) {
+      return '<@&' + data.rolesByName['legendary'].id + '> ';
+    } else {
+      console.warn('Please create a role called legendary.');
+    }
+  }
   return '';
 };
 
@@ -64,12 +64,12 @@ const getLegendaryTag = (boss, legendaries, data) => {
  * @returns {string}
  */
 const getSpecialRaidTag = (msglower, data) => {
-	if (msglower.indexOf('exgym') > -1 || msglower.indexOf(' ex gym') > -1 || msglower.indexOf('ex raid') > -1 || msglower.indexOf('(ex gym)') > -1) {
-		if (data.rolesByName['exgym']) {
-			return '<@&' + data.rolesByName['exgym'].id + '> ';
-		} else {
-			console.warn('Please create a role called exgym.');
-		}
+  if (msglower.indexOf('exgym') > -1 || msglower.indexOf(' ex gym') > -1 || msglower.indexOf('ex raid') > -1 || msglower.indexOf('(ex gym)') > -1) {
+    if (data.rolesByName['exgym']) {
+      return '<@&' + data.rolesByName['exgym'].id + '> ';
+    } else {
+      console.warn('Please create a role called exgym.');
+    }
   }
   return '';
 };
@@ -82,18 +82,18 @@ const getSpecialRaidTag = (msglower, data) => {
  */
 const getTierEmojiAndEggTag = (tier, data) => {
   let tierEmoji = '';
-	let eggTag = 'Tier ' + tier;
-	if (tier == 5) {
-		tierEmoji = 'legendaryraid';
-		eggTag = ' <@&' + data.rolesByName['tier5'].id + '> ';
-	} else if (tier < 3) {
-		tierEmoji = 'normalraid';
-		if(tier == 1) eggTag = ' <@&' + data.rolesByName['tier1'].id + '> ';
-		if(tier == 2) eggTag = ' <@&' + data.rolesByName['tier2'].id + '> ';		
-	} else if (tier > 2) {
-		tierEmoji = 'rareraid';
-		if(tier == 3) eggTag = ' <@&' + data.rolesByName['tier3'].id + '> ';
-		if(tier == 4) eggTag = ' <@&' + data.rolesByName['tier4'].id + '> ';
+  let eggTag = 'Tier ' + tier;
+  if (tier == 5) {
+    tierEmoji = 'legendaryraid';
+    eggTag = ' <@&' + data.rolesByName['tier5'].id + '> ';
+  } else if (tier < 3) {
+    tierEmoji = 'normalraid';
+    if(tier == 1) eggTag = ' <@&' + data.rolesByName['tier1'].id + '> ';
+    if(tier == 2) eggTag = ' <@&' + data.rolesByName['tier2'].id + '> ';    
+  } else if (tier > 2) {
+    tierEmoji = 'rareraid';
+    if(tier == 3) eggTag = ' <@&' + data.rolesByName['tier3'].id + '> ';
+    if(tier == 4) eggTag = ' <@&' + data.rolesByName['tier4'].id + '> ';
   }
   return { tierEmoji, eggTag };
 };
@@ -104,12 +104,12 @@ const getTierEmojiAndEggTag = (tier, data) => {
  * @returns {string}
  */
 const removeTags = html => {
-	let oldHtml;
-	do {
-		oldHtml = html;
-		html = html.replace(CONSTANTS.tagOrComment, '');
-	} while (html !== oldHtml);
-	return html.replace(/</g, '&lt;');
+  let oldHtml;
+  do {
+    oldHtml = html;
+    html = html.replace(CONSTANTS.tagOrComment, '');
+  } while (html !== oldHtml);
+  return html.replace(/</g, '&lt;');
 };
 
 /**
@@ -120,9 +120,9 @@ const removeTags = html => {
  */
 const sendAlertToChannel = (channelName, reply, data) => {
   if (data.channelsByName[channelName]) {
-		data.channelsByName[channelName].send(reply);
-	} else {
-		console.warn(`Please add a channel called #${channelName}`);
+    data.channelsByName[channelName].send(reply);
+  } else {
+    console.warn(`Please add a channel called #${channelName}`);
   }
 };
 
