de92e73ccbef348c0fa68c5a5186cc67007255df
Jeff <jeffreycleu@gmail.com>
2019-10-03 00:17:54 -0700

    Added a few more variations to stringsToRemove

--- src/helper.js
+++ src/helper.js
@@ -6,7 +6,21 @@ const CONSTANTS = require('./constants');
  * @returns {string}
  */
 const cleanUpDetails = detail => {
-	const stringsToRemove = new Set(['exgym ', 'ex gym ', 'shiny check ', 'shinycheck ', 'rarecandy ', 'candy ', 'silverpinap ', 'silver pinap', 'pinap ', 'technical ', 'technicalmachine ', 'technical machine ', 'finalevo ', 'highiv ']);
+	const exGymVariations = ['exgym ', 'ex gym ', 'ex raid ', '(exgym) ', '(ex gym) '];
+	const shinyCheckVariations = ['shiny check ', 'shinycheck '];
+	const rareCandyVariations = ['rarecandy ', 'candy '];
+	const silverPinapVariations = ['silverpinap ', 'silver pinap', 'pinap '];
+	const technicalMachineVariations = ['technical ', 'technicalmachine ', 'technical machine '];
+	const stringsToRemove = new Set([
+		...exGymVariations,
+		...shinyCheckVariations,
+		...rareCandyVariations,
+		...silverPinapVariations,
+		...technicalMachineVariations,
+		'finalevo ',
+		'highiv '
+		]);
+
 	stringsToRemove.forEach(string => {
 		const regex = new RegExp(string, 'gi');
 		detail = detail.replace(regex, '');
